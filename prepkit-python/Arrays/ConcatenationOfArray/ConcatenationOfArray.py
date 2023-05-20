a024de8af42b6be171fd7015de8f1155a43616ff
Ajay Shrestha <er.ajaystha@gmail.com>
2023-05-20 19:25:34 -0400

    1929. Concatenation of Array

---
+++ prepkit-python/Arrays/ConcatenationOfArray/ConcatenationOfArray.py
@@ -0,0 +1,9 @@
+# 1929. Concatenation of Array
+
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums + nums
+    
+    # Other Solution
+    # return num * 2
