51a74c1edc5b54299a17656965509f2d9508638a
Ajay Shrestha <er.ajaystha@gmail.com>
2023-05-23 20:05:31 -0400

    1481. Least Number of Unique Integers after K Removals

--- prepkit-python/Arrays/Medium/LeastNumberOfUniqueIntegersAfterKRemovals/LeastNumberOfUniqueIntegersAfterKRemovals.py
+++ prepkit-python/Arrays/Medium/LeastNumberOfUniqueIntegersAfterKRemovals/LeastNumberOfUniqueIntegersAfterKRemovals.py
@@ -1,1 +1,17 @@
 # 1481. Least Number of Unique Integers after K Removals
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        counter = Counter(arr)
+        sortVal = sorted(counter.values())
+        ans = len(sortVal)
+        for i in sortVal:
+            k -= i
+            if k < 0:
+                break
+            ans -= 1
+        return ans
+
+    # Other Solution
+        # c = Counter(arr)
+        # s = sorted(arr,key = lambda x:(c[x],x))
+        # return len(set(s[k:]))
