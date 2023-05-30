adae4187f17cfeae09e066ca9908cd2a620576f0
Ajay Shrestha <er.ajaystha@gmail.com>
2023-05-29 20:45:15 -0400

    1108. Defanging an IP Address

---
+++ prepkit-python/Strings/DefangingAnIPAddress/DefangingAnIPAddress.py
@@ -0,0 +1,8 @@
+# 1108. Defanging an IP Address
+
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        return address.replace('.', '[.]')
+    # Other solution
+        # return ('[.]').join(address.split('.'))
+        # return ('').join(['[.]' if el == '.' else el for el in address])
