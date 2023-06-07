5726c5e8016028a3c936bd68837eddcd9807aed4
Ajay Shrestha <er.ajaystha@gmail.com>
2023-06-07 15:26:47 -0400

    3. Longest Substring Without Repeating Characters

--- prepkit-python/Strings/Medium/ LongestSubstringWithoutRepeatingCharacters/ LongestSubstringWithoutRepeatingCharacters.py
+++ prepkit-python/Strings/Medium/ LongestSubstringWithoutRepeatingCharacters/ LongestSubstringWithoutRepeatingCharacters.py
@@ -2,22 +2,36 @@
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        char_set = set()
         p1 = 0
-        output = 0
-        obj = {}
-        if len(set(s)) == 1:
-            return 1
+        ans = 0
         for p2 in range(len(s)):
-            if s[p2] in obj:
-                if obj[s[p2]] < p1:
-                    diff = p2 - p1 + 1
-                    if output < diff:
-                        output = diff
-                else:
-                    p1 = obj[s[p2]] + 1
-            else:
-                diff = p2 - p1 + 1
-                if output < diff:
-                    output = diff
-            obj[s[p2]] = p2
-        return output
+            while s[p2] in char_set:
+                char_set.remove(s[p1])
+                p1 += 1
+            char_set.add(s[p2])
+            diff = p2 - p1 + 1
+            if ans < diff:
+                ans = diff
+        return ans
+
+        # Other Solution
+        # p1 = 0
+        # output = 0
+        # obj = {}
+        # if len(set(s)) == 1:
+        #     return 1
+        # for p2 in range(len(s)):
+        #     if s[p2] in obj:
+        #         if obj[s[p2]] < p1:
+        #             diff = p2 - p1 + 1
+        #             if output < diff:
+        #                 output = diff
+        #         else:
+        #             p1 = obj[s[p2]] + 1
+        #     else:
+        #         diff = p2 - p1 + 1
+        #         if output < diff:
+        #             output = diff
+        #     obj[s[p2]] = p2
+        # return output
