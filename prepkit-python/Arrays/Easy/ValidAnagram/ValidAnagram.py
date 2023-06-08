fde73c7b82d5b76c6b8c34fd1f13e40a86dbc44d
Ajay Shrestha <er.ajaystha@gmail.com>
2023-06-08 14:36:52 -0400

    242. Valid Anagram

--- prepkit-python/Arrays/Easy/ValidAnagram/ValidAnagram.py
+++ prepkit-python/Arrays/Easy/ValidAnagram/ValidAnagram.py
@@ -6,4 +6,5 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return Counter(s) == Counter(t)
 
-
+    # Other Solution
+    # return sorted(s) == sorted(t) - 0(1) memory complexity
