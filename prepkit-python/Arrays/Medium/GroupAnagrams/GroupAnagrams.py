16aa366927bfd618c0a67d041a1bd68bd9601f5a
Ajay Shrestha <er.ajaystha@gmail.com>
2023-06-08 15:49:19 -0400

    49. Group Anagrams

--- prepkit-python/Arrays/Medium/GroupAnagrams/GroupAnagrams.py
+++ prepkit-python/Arrays/Medium/GroupAnagrams/GroupAnagrams.py
@@ -2,11 +2,21 @@
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        obj = dict()
-        for el in strs:
-            sorted_word = ''.join(sorted(el))
-            if sorted_word in obj:
-                obj[sorted_word].append(el)
-            else:
-                obj[sorted_word] = [el]
-        return [i for i in obj.values()]
+        # Optimized Solution : time complexity = O(m * n)
+        obj = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            obj[tuple(count)].append(s)
+        return obj.values()
+
+        # Other solution
+        # obj = dict()  : Time complexity  = O(m * nlogn)
+        # for el in strs:
+        #     sorted_word = ''.join(sorted(el))
+        #     if sorted_word in obj:
+        #         obj[sorted_word].append(el)
+        #     else:
+        #         obj[sorted_word] = [el]
+        # return [i for i in obj.values()]
