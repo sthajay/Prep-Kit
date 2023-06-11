6860e0950f934a4288764ac958e7d22b700e431e
Ajay Shrestha <er.ajaystha@gmail.com>
2023-06-10 22:47:25 -0400

    128. Longest Consecutive Sequence

--- prepkit-python/Strings/Medium/LongestConsecutiveSequence/LongestConsecutiveSequence.py
+++ prepkit-python/Strings/Medium/LongestConsecutiveSequence/LongestConsecutiveSequence.py
@@ -2,20 +2,33 @@
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        unique_nums = set(nums)
-        n = len(unique_nums)
-        if n == 0 or n == 1:
-            return n
-        nums = sorted(unique_nums)
-        count = 0
-        obj = dict()
-        for i in range(n - 1):
-            if nums[i] + 1 == nums[i+1]:
-                if count in obj:
-                    obj[count] += 1
-                else:
-                    obj[count] = 1
-            else:
-                count += 1
-                obj[count] = 0
-        return max(obj.values()) + 1
+        # Time complexity = O(n)
+        # Space Complexity = O(n) for set
+        numSet = set(nums)
+        longest_sequence = 0
+        for el in nums:
+            if el - 1 not in numSet:
+                length = 0
+                while el + length in numSet:
+                    length += 1
+                longest_sequence = max(longest_sequence, length)
+        return longest_sequence
+
+        # Other Solution , Time complexity = O(nlogn)
+        # unique_nums = set(nums)
+        # n = len(unique_nums)
+        # if n == 0 or n == 1:
+        #     return n
+        # nums = sorted(unique_nums)
+        # count = 0
+        # obj = dict()
+        # for i in range(n - 1):
+        #     if nums[i] + 1 == nums[i+1]:
+        #         if count in obj:
+        #             obj[count] += 1
+        #         else:
+        #             obj[count] = 1
+        #     else:
+        #         count += 1
+        #         obj[count] = 0
+        # return max(obj.values()) + 1
