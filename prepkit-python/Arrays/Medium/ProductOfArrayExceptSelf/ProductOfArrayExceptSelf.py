c6448cc72652c0c161eb0ee9c36e7ac97b38fb59
Ajay Shrestha <er.ajaystha@gmail.com>
2023-06-11 11:31:42 -0400

    238. Product of Array Except Self

--- prepkit-python/Arrays/Medium/ProductOfArrayExceptSelf/ProductOfArrayExceptSelf.py
+++ prepkit-python/Arrays/Medium/ProductOfArrayExceptSelf/ProductOfArrayExceptSelf.py
@@ -3,17 +3,28 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        left = [0]*n
-        right = [0]*n
-        left[0] = 1
-        right[n-1] = 1
-        for i in range(1, n):
-            left[i] = left[i-1] * nums[i-1]
-            right[n-1-i] = right[n-i] * nums[n-i]
-        ans = []
+        ans = [1]*n
+        left = 1
+        right = 1
         for i in range(n):
-            ans.append(left[i] * right[i])
+            ans[i] *= left
+            left *= nums[i]
+            ans[n-i-1] *= right
+            right *= nums[n-i-1]
         return ans
+
+        # n = len(nums)
+        # left = [0]*n
+        # right = [0]*n
+        # left[0] = 1
+        # right[n-1] = 1
+        # for i in range(1, n):
+        #     left[i] = left[i-1] * nums[i-1]
+        #     right[n-1-i] = right[n-i] * nums[n-i]
+        # ans = []
+        # for i in range(n):
+        #     ans.append(left[i] * right[i])
+        # return ans
 
     # Other Solution
         # length=len(nums)
