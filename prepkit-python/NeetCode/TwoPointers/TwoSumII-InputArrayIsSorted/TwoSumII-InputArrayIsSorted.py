35f6e9796ac80f4b5f96a93161f65995d19554ef
Ajay Shrestha <er.ajaystha@gmail.com>
2023-06-28 00:33:59 -0400

    167. Two Sum II - Input Array Is Sorted

--- prepkit-python/NeetCode/TwoPointers/TwoSumII-InputArrayIsSorted/TwoSumII-InputArrayIsSorted.py
+++ prepkit-python/NeetCode/TwoPointers/TwoSumII-InputArrayIsSorted/TwoSumII-InputArrayIsSorted.py
@@ -1,10 +1,24 @@
 # 167. Two Sum II - Input Array Is Sorted
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        obj = {}
-        for i in range(len(numbers)):
-            diff = target - numbers[i]
-            if numbers[i] in obj:
-                return [obj[numbers[i]] + 1, i+1]
+        # Time Complexity = O(n) Linear
+        # Space Complexity = O(1) Constant extra space
+        l, r = 0, len(numbers) - 1
+        while l < r:
+            total_sum = numbers[l] + numbers[r]
+            if total_sum == target:
+                return [l + 1, r + 1]
+            elif total_sum > target:
+                r -= 1
             else:
-                obj[diff] = i
+                l += 1
+        return []
+
+        # Time and Memory Complexity = O(n)
+        # obj = {}
+        # for i in range(len(numbers)):
+        #     diff = target - numbers[i]
+        #     if numbers[i] in obj:
+        #         return [obj[numbers[i]] + 1, i+1]
+        #     else:
+        #         obj[diff] = i
