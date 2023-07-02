7597b450742c62859a10d69375ec5faa55f183da
Ajay Shrestha <er.ajaystha@gmail.com>
2023-07-02 18:55:16 -0400

    21. Merge Two Sorted Lists

--- prepkit-python/NeetCode/LinkedList/MergeTwoSortedLists/MergeTwoSortedLists.py
+++ prepkit-python/NeetCode/LinkedList/MergeTwoSortedLists/MergeTwoSortedLists.py
@@ -1,1 +1,35 @@
 # 21. Merge Two Sorted Lists
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+
+        if list1:
+            tail.next = list1
+        elif list2:
+            tail.next = list2
+
+        return dummy.next
+
+
+'''
+We have created a dummy object of the class. 
+So, when we return head we will have one more extra node 
+in the final value which is actually dummy node. 
+If you see the result object you will see the answer is [1,1,2,3,4,4] 
+but if you return the head which we have started using dummy node the
+result will be [0,1,1,2,3,4,4]. Thus, we return head.next
+ '''
