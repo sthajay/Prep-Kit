9f2a1cb2eed99593c64d7d7e7d569bee95fd72e7
Ajay Shrestha <er.ajaystha@gmail.com>
2023-07-01 23:15:00 -0400

    16. 3Sum Closest

--- prepkit-python/Arrays/Medium/3SumClosest/3SumClosest.py
+++ prepkit-python/Arrays/Medium/3SumClosest/3SumClosest.py
@@ -1,6 +1,35 @@
 # 16. 3Sum Closest
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
+        '''
+        The overall time complexity is O(nlogn + n^2), which simplifies to O(n^2) 
+        since the n^2 term dominates the nlogn term.
+
+        The space complexity of this code is O(1) since it uses a constant amount of 
+        additional space. The variables ans, initial_diff, l, r, threeSum, 
+        and diff are the only variables used, and they occupy a constant
+        amount of space regardless of the size of the input.
+        '''
+        nums.sort()
+        ans = 0
+        initial_diff = float('inf')
+        for i, el in enumerate(nums):
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threeSum = el + nums[l] + nums[r]
+                if threeSum > target:
+                    r -= 1
+                elif threeSum < target:
+                    l += 1
+                else:
+                    return threeSum
+                diff = abs(target - threeSum)
+                if diff < initial_diff:
+                    initial_diff = diff
+                    ans = threeSum
+        return ans
+
+        # Other Solution
         min_diff = 10000000
         closest_sum = 0
         n = len(nums)
