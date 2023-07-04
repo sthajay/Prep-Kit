bad9f88bdb8e478580a4b4c5555e3738f74d9923
Ajay Shrestha <er.ajaystha@gmail.com>
2023-07-03 21:57:17 -0400

    11. Container With Most Water

--- prepkit-python/NeetCode/TwoPointers/ContainerWithMostWater/ContainerWithMostWater.py
+++ prepkit-python/NeetCode/TwoPointers/ContainerWithMostWater/ContainerWithMostWater.py
@@ -1,16 +1,28 @@
 # 11. Container With Most Water
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        max_area = 0
-        p1, p2 = 0, len(height) - 1
-        while p1 < p2:
-            if height[p1] > height[p2]:
-                max_area = max(max_area, height[p2] * (p2 - p1))
-                p2 -= 1
+        l, r = 0, len(height) - 1
+        ans = 0
+        while l < r:
+            max_area = (r - l) * min(height[l], height[r])
+            ans = max(ans, max_area)
+            if height[l] < height[r]:
+                l += 1
             else:
-                max_area = max(max_area, height[p1] * (p2 - p1))
-                p1 += 1
-        return max_area
+                r -= 1
+        return ans
+
+        # Other Solution
+        # max_area = 0
+        # p1, p2 = 0, len(height) - 1
+        # while p1 < p2:
+        #     if height[p1] > height[p2]:
+        #         max_area = max(max_area, height[p2] * (p2 - p1))
+        #         p2 -= 1
+        #     else:
+        #         max_area = max(max_area, height[p1] * (p2 - p1))
+        #         p1 += 1
+        # return max_area
 
         # Brute force solution , Time complexity = O(n^2)
         # max_area = 0
